81f96eccea8e0109feefc4be98254c1f54991150
Jason Shen <jason_2000_nz@hotmail.com>
2023-06-07 18:04:59 +1200

    added the missing declare for typescript

--- index.d.ts
+++ index.d.ts
@@ -5,9 +5,13 @@ declare module 'react-native-voip-push-notification' {
     'didLoadWithEvents';
 
     export default class RNVoipPushNotification {
-        static addEventListener(type: Events, handler: (args: any) => void): void
+        static RNVoipPushRemoteNotificationsRegisteredEvent: string;
+        static RNVoipPushRemoteNotificationReceivedEvent: string;
+        static RNVoipPushDidLoadWithEvents: string;
+
+        static addEventListener(type: Events, handler: (args: any) => void): void;
         static registerVoipToken(): void;
         static onVoipNotificationCompleted(uuid: string): void;
-        static removeEventListener(type: Events): void
+        static removeEventListener(type: Events): void;
     }
 }
